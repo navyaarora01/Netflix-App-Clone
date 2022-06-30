30d497465eba507e578249778b24125f508846ea
Navya <arora.navya2003@gmail.com>
2022-06-30 23:56:38 +0530

    commit

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,19 +1,20 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAOj-3-HiLlSR18SAffy-2sexf6RBDtvm4",
-  authDomain: "netflix-app-clone-a89cd.firebaseapp.com",
-  projectId: "netflix-app-clone-a89cd",
-  storageBucket: "netflix-app-clone-a89cd.appspot.com",
-  messagingSenderId: "1024730880031",
-  appId: "1:1024730880031:web:05504a6e6addb3de34dd2a"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAOj-3-HiLlSR18SAffy-2sexf6RBDtvm4",
+//   authDomain: "netflix-app-clone-a89cd.firebaseapp.com",
+//   projectId: "netflix-app-clone-a89cd",
+//   storageBucket: "netflix-app-clone-a89cd.appspot.com",
+//   messagingSenderId: "1024730880031",
+//   appId: "1:1024730880031:web:05504a6e6addb3de34dd2a"
+// };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
