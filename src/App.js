344a4d1eaef586d4d6fa77f6f591b5d4d87b483c
Navya <arora.navya2003@gmail.com>
2022-07-01 12:13:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { Route , Routes} from "react-router-dom";
-function App(){
-    return(
-         <> 
-         <Navbar />
-        
-         <Routes>
-            <Route path='/' element={<Home />} />
-         </Routes>
-        </>
-    
-    );
-       
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
 }
 export default App;
--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,20 +1,23 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAOj-3-HiLlSR18SAffy-2sexf6RBDtvm4",
-  authDomain: "netflix-app-clone-a89cd.firebaseapp.com",
-  projectId: "netflix-app-clone-a89cd",
-  storageBucket: "netflix-app-clone-a89cd.appspot.com",
-  messagingSenderId: "1024730880031",
-  appId: "1:1024730880031:web:05504a6e6addb3de34dd2a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
